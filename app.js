eaa5dded3f13267d5db5d4d70166676a1449c2ff
carlosbohorquez <44281235+owakester@users.noreply.github.com>
2022-03-31 12:56:09 -0300

    formato app.js

--- app.js
+++ app.js
@@ -5,25 +5,20 @@ const url =
 
 const contenedorCarrito = document.querySelector("#descripcion tbody");/* Despliega info de JSON en table. */
 const agregarP = document.querySelector("#agregar tbody");
-      
+
 let carrito = [
-  
 ];
+
 function agregando(i) {
-
-  let hAtribute=document.getElementById("mas").getAttribute("data-id").innerHTML=i;
-
-
-document.getElementById("prueba").innerHTML=hAtribute;
-  } 
+  let hAtribute = document.getElementById("mas").getAttribute("data-id").innerHTML = i;
+  document.getElementById("prueba").innerHTML = hAtribute;
+}
 
 fetch(url)
   .then((response) => {
     return response.json();
   })
-
   .then((jsondata) => {
-    /* debugger */
     let pagina1 = 10;
     let i = 0;
 
@@ -32,60 +27,38 @@ fetch(url)
     function ejecutar() {
       var table = $("#descripcion > tbody > tr > td ").remove();
       for (i; i < jsondata.data.length; i++) {
-      /*   console.log("registros" + i); */
-
         const row = document.createElement("tr");
         const agregar = document.createElement("tr");
-    /*     let ids= document.createAttribute("data-id="); */
-       /*  debugger; */
-       let articulos={
-        nombre:jsondata.data[i].Producto,
-        medida:jsondata.data[i].Medida,
-        precio:jsondata.data[i].Precio,
-      
-    }
+
+        let articulos = {
+          nombre: jsondata.data[i].Producto,
+          medida: jsondata.data[i].Medida,
+          precio: jsondata.data[i].Precio,
+
+        }
         row.innerHTML = `
-      
-             <td><button onclick="agregando(${i})"data-id="${articulos.id=i}" id="mas" class="btn btn-blue"><ion-icon size="large" name="add-circle-outline"></ion-icon></button>${
-         articulos.nombre
-      
-        } </td>
-      
+             <td><button onclick="agregando(${i})"data-id="${articulos.id = i}" id="mas" class="btn btn-blue"><ion-icon size="large" name="add-circle-outline"></ion-icon></button>${articulos.nombre} </td>
              <td>${articulos.medida} </td>
              <td>${articulos.precio} </td>
-           
         `;
-   /* console.log(carrito) */
-   carrito.push(articulos)
-
+        /* console.log(carrito) */
+        carrito.push(articulos)
         contenedorCarrito.appendChild(row);
         agregarP.appendChild(agregar);
 
         if (i == pagina1) {
           console.log(pagina1);
-
           siguiente(pagina1);
           pagina1 = pagina1 + 10;
-
           break;
         }
       }//for
-      
-    
     }
 
     function siguiente(pagina1) {
-      /*  console.log("hola  "+pagina1) */
-
       document.getElementById("btn").addEventListener("click", ejecutar);
     }
-
-
-
-
-
-
   });
 
-  
-  
+
+
